f71b7bf1d4bf6b85cfabfc7b495754c79f0d4055
Brandon Askea <brandon.askea@me.com>
2019-03-01 00:48:18 -0800

    Added MGCard

---
+++ Memory Game/MGCard.swift
@@ -0,0 +1,30 @@
+//
+//  MGCard.swift
+//  Memory Game
+//
+//  Created by Brandon Askea on 3/1/19.
+//  Copyright © 2019 Brandon Askea. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MGCard: SKSpriteNode {
+    
+    private var type: MGCardType = .unknown
+    
+    init(size: CGSize, cardType: MGCardType) {
+        super.init(texture: nil, color: .clear, size: size)
+        updateWith(cardType: type)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func updateWith(cardType: MGCardType) {
+        type = cardType
+        texture = SKTexture(imageNamed: "card_\(type.rawValue)")
+    }
+    
+}
---
+++ Memory Game/MGCardType.swift
@@ -0,0 +1,13 @@
+//
+//  MGCardType.swift
+//  Memory Game
+//
+//  Created by Brandon Askea on 3/1/19.
+//  Copyright © 2019 Brandon Askea. All rights reserved.
+//
+
+import Foundation
+
+enum MGCardType: Int {
+    case unknown = 0, bat, cat, cow, dragon, garbageMan, ghost, hen, horse, pig, spider
+}
